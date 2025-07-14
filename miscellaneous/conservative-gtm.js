dd5960a6777ccbf444afc7117ac02542df5b6879
ErwinHofmanRV <erwin@rumvision.com>
2025-07-14 17:40:28 +0200

    conservative GTM for prerendered pages

---
+++ miscellaneous/conservative-gtm.js
@@ -0,0 +1,20 @@
+let navigationType = null;
+function conservativeGTM(e) {
+	if (navigationType) {
+		return;
+	}
+	if (document.prerendering) {
+		/* let's try again later */
+		document.addEventListener('prerenderingchange', arguments.callee, { once: true });
+		return;
+	}
+
+	navigationType = e?.type || performance.getEntriesByType('navigation')[0]?.type || 'navigate';
+
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-CODE');
+}
+conservativeGTM();
