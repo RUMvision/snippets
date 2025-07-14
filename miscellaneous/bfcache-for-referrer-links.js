94153d0c7922f0bb6710e300b2d81c7a2a92316c
ErwinHofmanRV <erwin@rumvision.com>
2025-07-14 17:22:58 +0200

    bfcache for referrer links

---
+++ miscellaneous/bfcache-for-referrer-links.js
@@ -0,0 +1,17 @@
+/*
+This JavaScript code snippet is designed to improve the user experience by leveraging the Back-Forward Cache (bfcache) for navigation when a user clicks a link that would effectively take them back to the previous page.
+/*
+(function(ref) {
+	if (!ref) { return };
+
+	// leverage bfcache for hard links that would go back one step anyway
+	const refUrl = new URL(ref);
+	const selector = 'a[href="' + refUrl.pathname + '"],a[href="' + refUrl.href + '"]';
+	// query by relative and absolute links
+	document.querySelectorAll(selector).forEach(link => {
+		link.addEventListener('click', e => {
+			e.preventDefault();
+			history.go(-1);
+		});
+	});
+})(document.referrer);
