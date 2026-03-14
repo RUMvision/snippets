b6cc9f50a179f7de04f164f0269690640a404a0c
ErwinHofmanRV <erwin@rumvision.com>
2026-03-14 12:32:27 +0100

    added prerender_until_script origin trial setup

---
+++ speculation-rules/prerender_until_script-origin-trial.js
@@ -0,0 +1,76 @@
+// More info:
+// https://www.rumvision.com/blog/prerender-until-script-in-between-prefetch-and-prerender/
+
+function enableChromeOriginTrial(token, until = '2026-09-08') {
+
+	// Skip if token is missing
+	if (!token) {
+		return false;
+	}
+
+	// Skip if origin trial expired
+	if (Date.now() > new Date(until).getTime()) {
+		return false;
+	}
+
+	// Detect Google Chrome
+	const ua = navigator.userAgent;
+	const isChrome =
+		navigator.vendor === 'Google Inc.' &&
+		ua.includes('Chrome') &&
+		!ua.includes('Edg') &&      // Edge
+		!ua.includes('OPR') &&      // Opera
+		!ua.includes('SamsungBrowser');
+
+	if (!isChrome) {
+		return false;
+	}
+
+	// Inject origin trial token
+	const meta = document.createElement('meta');
+	meta.httpEquiv = 'origin-trial';
+	meta.content = token;
+
+	document.head.appendChild(meta);
+
+	return true;
+}
+
+
+// Get your own token at:
+// https://developer.chrome.com/origintrials/#/view_trial/881016677104353281
+const originTrialToken = '';
+
+(function (specType, eagerness) {
+
+	// Feature detection
+	if (!HTMLScriptElement.supports || !HTMLScriptElement.supports('speculationrules')) {
+		return;
+	}
+
+	// Generate speculation rules
+	// Tailor this to the needs of your site/shop
+	// via https://lab.rumvision.com/other/api/speculation-rules/generator/
+	const rules = {
+		[specType]: [{
+			tag: specType + '+' + eagerness,
+			eagerness,
+			where: {
+				and: [
+					{ href_matches: '/*' },
+					{ not: { selector_matches: '[data-noprefetch]' } }
+				]
+			}
+		}]
+	};
+
+	const script = document.createElement('script');
+	script.type = 'speculationrules';
+	script.textContent = JSON.stringify(rules);
+
+	document.head.appendChild(script);
+
+})(
+	enableChromeOriginTrial(originTrialToken) ? 'prerender_until_script' : 'prefetch',
+	'conservative'
+);
